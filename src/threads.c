36daacd194487e908fb085c0ca8e618090c6dc59
Dean Ruina <druina@c3r1p1.hive.fi>
2023-08-16 17:21:19 +0300

    no bottle neck, but data race

--- src/threads.c
+++ src/threads.c
@@ -6,7 +6,7 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:01:57 by druina            #+#    #+#             */
-/*   Updated: 2023/08/16 16:28:14 by druina           ###   ########.fr       */
+/*   Updated: 2023/08/16 17:17:41 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,11 @@ void	eat(t_philo *philo)
 	pthread_mutex_lock(philo->l_fork);
 	print_message("has taken a fork", philo, philo->id);
 	philo->eating = 1;
-	pthread_mutex_lock(philo->meal_lock);
+	// pthread_mutex_lock(philo->meal_lock);
 	philo->last_meal = get_current_time();
 	print_message("is eating", philo, philo->id);
 	philo->meals_eaten++;
-	pthread_mutex_unlock(philo->meal_lock);
+	// pthread_mutex_unlock(philo->meal_lock);
 	ft_usleep(philo->time_to_eat);
 	philo->eating = 0;
 	pthread_mutex_unlock(philo->l_fork);
